b60caee77c80fb16bf9e5abe2d1bf793dc74b4c1
Vinícius Tabille Manjabosco <thevtm@gmail.com>
2016-01-25 22:48:32 -0200

    Added support for ES2015 through babelify

--- component/main/Main.jsx
+++ component/main/Main.jsx
@@ -2,7 +2,7 @@ var React = require( 'react' );
 
 var Main = React.createClass( {
 	displayName: 'Main',
-	render: function() {
+	render: () => {
 		return (
 			<div>
 				<h1>nw.js meets React.js</h1>
--- gulp/browserify.js
+++ gulp/browserify.js
@@ -1,15 +1,16 @@
 var gulp = require( 'gulp' );
 var browserify = require( 'browserify' );
-var reactify = require( 'reactify' );
+var babelify = require( 'babelify' );
 var source = require( 'vinyl-source-stream' );
 
 gulp.task( 'browserify', function () {
 	var bundler = browserify( {
 		entries: [ './component/app.jsx' ],
 		extensions: [ '.jsx' ],
-		transform: [ reactify ],
 		debug: ( process.env.NODE_ENV === 'production' ) ? false : true
-	} );
+	} )
+		.transform(babelify, {presets: ["es2015", "react"]});
+
 	return bundler.bundle()
 		.pipe( source( 'app.js' ) )
 		.pipe( gulp.dest( './compile' ) );
